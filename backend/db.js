be041c0aa5404c1dab6a9e5464f091124f7a578d
Roshan <its.roshanojha@gmail.com>
2022-11-29 10:38:18 +0530

    Connected to DB and installed nodemon

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+
+const mongoURI = 'mongodb://localhost:27017'
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, ()=>{
+        console.log('connected to mongo Successfully')
+    })
+}
+
+module.exports = connectToMongo
