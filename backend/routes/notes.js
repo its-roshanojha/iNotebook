4cf75aa3ea065b3276820ca68d82b8067a418eb4
Roshan <its.roshanojha@gmail.com>
2022-12-04 11:55:35 +0530

    Fetching notes after authenticating user

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser =require('../middleware/fetchuser');
+const Notes = require('../models/Notes');
 
-router.get('/', (req, res)=>{
-   
-    res.json([])
+// ROUTE 1 : Get All the Notes using : GET "/api/auth/getuser" .Login required
+router.get('/fetchallnotes', fetchuser, async (req, res)=>{
+   const notes = await Notes.find({user: req.user.id});
+   res.json(notes)
 })
+
 
 module.exports = router;
