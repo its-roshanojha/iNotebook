3936e2b43137c1c8e70ec23a83bfba18907a34a2
Roshan <its.roshanojha@gmail.com>
2022-12-04 12:21:28 +0530

    Adding a note after authenticating user

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,13 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetchuser =require('../middleware/fetchuser');
-const Notes = require('../models/Notes');
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-// ROUTE 1 : Get All the Notes using : GET "/api/auth/getuser" .Login required
-router.get('/fetchallnotes', fetchuser, async (req, res)=>{
-   const notes = await Notes.find({user: req.user.id});
-   res.json(notes)
-})
+// ROUTE 1 : Get All the Notes using : GET "/api/notes/getuser" .Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+// ROUTE 2 : Add a new note using : POST "/api/notes/addnote" .Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
 
+      // If there are errors, return bad req and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      // to send the saved note in the response
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 module.exports = router;
